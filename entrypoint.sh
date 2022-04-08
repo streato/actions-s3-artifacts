cb8e41df36c57da7fa71ef6a2fc1777f8711b7df
Streato <83213269+streato@users.noreply.github.com>
2022-04-08 22:24:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,11 @@ if [ -z "$INPUT_S3_REGION" ]; then
   exit 1
 fi
 
+if [ -z "$INPUT_S3_ENDPOINT_URL" ]; then
+  echo "S3_ENDPOINT_URL is not set. Quitting."
+  exit 1
+fi
+
 if [ -z "$INPUT_S3_STORAGE_CLASS" ]; then
   INPUT_S3_STORAGE_CLASS="STANDARD"
 fi
@@ -42,7 +47,7 @@ endpoint = awscli_plugin_endpoint
 [default]
 region = fr-par
 s3 =
-  endpoint_url = https://s3.fr-par.scw.cloud
+  endpoint_url = ${INPUT_S3_ENDPOINT_URL}
   signature_version = s3v4
   max_concurrent_requests = 100
   max_queue_size = 1000
@@ -50,7 +55,7 @@ s3 =
   # Edit the multipart_chunksize value according to the file sizes that you want to upload. The present configuration allows to upload files up to 10 GB (1000 requests * >
   multipart_chunksize = 10MB
 s3api =
-  endpoint_url = https://s3.fr-par.scw.cloud
+  endpoint_url = ${INPUT_S3_ENDPOINT_URL}
 EOF
 
 aws --storage-class=${INPUT_S3_STORAGE_CLASS} s3 cp ${INPUT_SOURCE_DIR} s3://${INPUT_S3_BUCKET}
