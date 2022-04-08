85dfb1b959f7994902c75c08800f1ff1c130cd3a
Streato <83213269+streato@users.noreply.github.com>
2022-04-08 22:51:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,8 +27,12 @@ if [ -z "$INPUT_S3_ENDPOINT_URL" ]; then
   exit 1
 fi
 
+if [ -z "$INPUT_FOLDER" ]; then
+  INPUT_FOLDER=""
+fi
+
 if [ -z "$INPUT_S3_STORAGE_CLASS" ]; then
-  INPUT_S3_STORAGE_CLASS="STANDARD"
+  INPUT_S3_STORAGE_CLASS=""
 fi
 
 aws configure <<-EOF > /dev/null 2>&1
@@ -38,6 +42,6 @@ ${INPUT_S3_REGION}
 text
 EOF
 
-aws --endpoint-url=${INPUT_S3_ENDPOINT_URL} --storage-class=${INPUT_S3_STORAGE_CLASS} s3 cp ${INPUT_SOURCE_DIR} s3://${INPUT_S3_BUCKET}
+aws --endpoint-url=${INPUT_S3_ENDPOINT_URL} --storage-class=${INPUT_S3_STORAGE_CLASS} s3 cp ${INPUT_SOURCE_DIR} s3://${INPUT_S3_BUCKET} ${INPUT_FOLDER}
 
 rm -r ~/.aws
