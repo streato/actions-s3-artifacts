edf70c11c9b9b77cc5895a47c30d1e18daa8089a
Streato <83213269+streato@users.noreply.github.com>
2022-04-08 16:52:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,42 @@
 
 set -e
 
+if [ -z "$S3_BUCKET" ]; then
+  echo "S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$S3_ACCESS_KEY_ID" ]; then
+  echo "S3_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$S3_REGION" ]; then
+  echo "S3_REGION is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$S3_ENDPOINT_URL" ]; then
+  echo "S3_ENDPOINT_URL is not set. Quitting."
+  exit 1
+fi
+
+# Override default AWS endpoint if user sets S3_STORAGE_CLASS.
+if [ -n "$S3_STORAGE_CLASS" ]; then
+  STORAGE_CLASS_APPEND="--storage-class $S3_STORAGE_CLASS"
+fi
+
 aws configure set plugins.endpoint awscli_plugin_endpoint
 
 aws configure <<-EOF > /dev/null 2>&1
-SCWBBWNPAD48GCCVTW66
-8e2a0f85-0f44-45ee-92b4-0ea9dd2dc170
-fr-par
+${S3_ACCESS_KEY_ID}
+${S3_SECRET_ACCESS_KEY}
+${S3_REGION}
 text
 EOF
 
@@ -17,9 +47,9 @@ cat <<'EOF' >> ~/.aws/config
 [plugins]
 endpoint = awscli_plugin_endpoint
 [default]
-region = fr-par
+region = ${S3_REGION}
 s3 =
-  endpoint_url = https://s3.fr-par.scw.cloud
+  endpoint_url = ${S3_ENDPOINT_URL}
   signature_version = s3v4
   max_concurrent_requests = 100
   max_queue_size = 1000
@@ -27,9 +57,9 @@ s3 =
   # Edit the multipart_chunksize value according to the file sizes that you want to upload. The present configuration allows to upload files up to 10 GB (1000 requests * >
   multipart_chunksize = 10MB
 s3api =
-  endpoint_url = https://s3.fr-par.scw.cloud
+  endpoint_url = ${S3_ENDPOINT_URL}
 EOF
 
-aws --storage-class=GLACIER s3 cp public s3://streato --recursive
+aws ${STORAGE_CLASS_APPEND} s3 cp ${SOURCE_DIR} s3:// --recursive
 
 rm -r ~/.aws
