cc698fe42c58ffc263896a125d8299d306171b8a
Streato <83213269+streato@users.noreply.github.com>
2022-04-08 15:44:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,36 @@
+#!/bin/sh
 
+set -e
+
+aws configure set plugins.endpoint awscli_plugin_endpoint
+
+aws configure <<-EOF > /dev/null 2>&1
+SCWBBWNPAD48GCCVTW66
+8e2a0f85-0f44-45ee-92b4-0ea9dd2dc170
+fr-par
+text
+EOF
+
+rm ~/.aws/config
+
+cat <<'EOF' >> ~/.aws/config
+[plugins]
+endpoint = awscli_plugin_endpoint
+
+[default]
+region = nl-ams
+s3 =
+  endpoint_url = https://streato.s3.fr-par.scw.cloud
+  signature_version = s3v4
+  max_concurrent_requests = 100
+  max_queue_size = 1000
+  multipart_threshold = 50MB
+  # Edit the multipart_chunksize value according to the file sizes that you want to upload. The present configuration allows to upload files up to 10 GB (1000 requests * 10MB). For example setting it to 5GB allows you to upload files up to 5TB.
+  multipart_chunksize = 10MB
+s3api =
+  endpoint_url = https://streato.s3.fr-par.scw.cloud
+EOF
+
+aws s3 cp public/logo.png s3://test
+
+rm -r ~/.aws
